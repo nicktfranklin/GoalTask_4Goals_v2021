0875a2be69e633a7964bbcb14ef4c1e5b5e298da
Nick Franklin <nthompsonfranklin@gmail.com>
2021-08-02 20:23:12 -0400

    initial commit

---
+++ static/js/utils.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function AssertException(message) { this.message = message; }
+AssertException.prototype.toString = function () {
+	return 'AssertException: ' + this.message;
+};
+
+function assert(exp, message) {
+	if (!exp) {
+		throw new AssertException(message);
+	}
+}
